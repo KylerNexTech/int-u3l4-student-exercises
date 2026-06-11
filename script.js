ccc32bf14c7d979377e30d5409a671bd39556d0c
KylerNexTech <Kylersmith1101@gmail.com>
2026-06-11 12:38:58 -0700

    Assignment is complete.

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ let img = document.querySelector("img");
 // - Make it so that the header says your name when the button is clicked.
 // - Change the src of the image to something random.
 button.addEventListener("click", function() {
-    header.innerHTML = "🤯 wow cool neeto";
-    img.src = "https://media4.giphy.com/media/26ufdipQqU2lhNA4g/giphy.gif";
+    header.innerHTML = "Kyler😎";
+    img.src = "https://media1.tenor.com/m/k793FJTjnrMAAAAd/deepwoken-midwoken.gif";
 });
 
 
